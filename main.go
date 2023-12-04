842c5b372bef2669ea2341984d629ff452875193
maula iqbal <rifqimaula@gmail.com>
2023-12-04 14:52:03 +0700

    oke

--- main.go
+++ main.go
@@ -1,42 +1,46 @@
 package main
 
 import (
-	"github.com/iqbaltc13/back-end-learner-tryout-api/controllers"
+	"fmt"
+	"log"
 
 	"github.com/gin-gonic/gin"
+	"github.com/iqbaltc13/back-end-learner-tryout-api/database"
+	"github.com/iqbaltc13/back-end-learner-tryout-api/middleware"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	loadEnv()
 	loadDatabase()
 	serveApplication()
-	func loadEnv() {
-		err := godotenv.Load(".env.local")
-		if err != nil {
-			log.Fatal("Error loading .env file")
-		}
+
+}
+func loadEnv() {
+	err := godotenv.Load(".env.local")
+	if err != nil {
+		log.Fatal("Error loading .env file")
 	}
-	
-	func loadDatabase() {
-		database.Connect()
-		//database.Database.AutoMigrate(&model.User{})
-		//database.Database.AutoMigrate(&model.Entry{})
-	}
-	
-	func serveApplication() {
-		router := gin.Default()
-	
-		publicRoutes := router.Group("/auth")
-		publicRoutes.POST("/auth/register", controller.Register)
-		publicRoutes.POST("/auth/login", controller.Login)
-	
-		protectedRoutes := router.Group("/api")
-		protectedRoutes.Use(middleware.JWTAuthMiddleware())
-		protectedRoutes.POST("/entry", controller.AddEntry)
-		protectedRoutes.GET("/entry", controller.GetAllEntries)
-	
-		router.Run(":8000")
-		fmt.Println("Server running on port 8000")
-	}
-	
+}
+
+func loadDatabase() {
+	database.Connect()
+	//database.Database.AutoMigrate(&model.User{})
+	//database.Database.AutoMigrate(&model.Entry{})
+}
+
+func serveApplication() {
+	router := gin.Default()
+
+	publicRoutes := router.Group("/auth")
+	publicRoutes.POST("/auth/register", controller.Register)
+	publicRoutes.POST("/auth/login", controller.Login)
+
+	protectedRoutes := router.Group("/api")
+	protectedRoutes.Use(middleware.JWTAuthMiddleware())
+	protectedRoutes.POST("/entry", controller.AddEntry)
+	protectedRoutes.GET("/entry", controller.GetAllEntries)
+
+	router.Run(":8000")
+	fmt.Println("Server running on port 8000")
 }
