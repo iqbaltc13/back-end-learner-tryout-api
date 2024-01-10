5b5a93a780a46226aad5a5d87337614b6389b149
maula iqbal <rifqimaula@gmail.com>
2024-01-10 13:59:15 +0700

    oke

--- models/user.go
+++ models/user.go
@@ -21,11 +21,11 @@ type User struct {
 	TokenLoginMobile      string `gorm:"size:1000;not null;column:token_login_mobile" json:"token_login_mobile"`
 	CurrentApkVersionName string `gorm:"size:1000;not null;column:current_apk_version_name" json:"current_apk_version_name"`
 	CurrentApkVersionCode string `gorm:"size:1000;not null;column:current_apk_version_code" json:"current_apk_version_code"`
-	VerifiedAt            string `gorm:"size:1000;not null;column:email_verified_at" json:"email_verified_at"`
+	VerifiedAt            string `gorm:"size:1000;null;column:email_verified_at" json:"email_verified_at"`
 	DeviceInfo            string `gorm:"size:1000;not null;column:device_info" json:"device_info"`
-	DeletedAt             string `gorm:"size:1000;not null;column:deleted_at" json:"deleted_at"`
-	CreatedAt             string `gorm:"size:1000;not null;column:created_at" json:"created_at"`
-	UpdatedAt             string `gorm:"size:1000;not null;column:updated_at" json:"updated_at"`
+	DeletedAt             string `gorm:"size:1000;null;column:deleted_at" json:"deleted_at"`
+	CreatedAt             string `gorm:"size:1000;null;column:created_at" json:"created_at"`
+	UpdatedAt             string `gorm:"size:1000;null;column:updated_at" json:"updated_at"`
 
 	Entries []Entry
 }
