caf5381f73bd1679b12bec68927728164bdf2000
maula iqbal <rifqimaula@gmail.com>
2024-01-26 15:59:54 +0700

    oke

--- controllers/home.go
+++ controllers/home.go
@@ -2,13 +2,21 @@ package controllers
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/iqbaltc13/back-end-learner-tryout-api/helper"
 )
 
 func Home(context *gin.Context) {
-	token := helper.ValidateJWT(context)
+	token, err := helper.CurrentUser(context)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"response_code": 500,
+			"error":         err.Error(),
+		})
+		return
+	}
 
 	fmt.Println(token)
 }
