d8883055428633b738007beb5dc1714dc4f2d3d5
maula iqbal <rifqimaula@gmail.com>
2024-01-26 14:09:31 +0700

    oke

--- main.go
+++ main.go
@@ -7,11 +7,21 @@ import (
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt"
 	"github.com/iqbaltc13/back-end-learner-tryout-api/controllers"
 	"github.com/iqbaltc13/back-end-learner-tryout-api/database"
 	"github.com/iqbaltc13/back-end-learner-tryout-api/middleware"
 	"github.com/joho/godotenv"
 )
+
+type M map[string]interface{}
+
+type MyClaims struct {
+	jwt.StandardClaims
+	Username string `json:"Username"`
+	Email    string `json:"Email"`
+	Group    string `json:"Group"`
+}
 
 func main() {
 	loadEnv()
