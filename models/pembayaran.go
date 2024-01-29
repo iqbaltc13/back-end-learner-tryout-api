87f27c9bd3c89885f145ba5063fd694a95c991c8
maula iqbal <rifqimaula@gmail.com>
2024-01-29 17:00:41 +0700

    oke

---
+++ models/pembayaran.go
@@ -0,0 +1,28 @@
+package models
+
+import (
+	"github.com/iqbaltc13/back-end-learner-tryout-api/database"
+
+	"gorm.io/gorm"
+)
+
+type Pembayaran struct {
+	gorm.Model
+	ID        string `gorm:"primaryKey"`
+	Userid    string `gorm:"not null;column:user_id" json:"user_id"`
+	Classid   string `gorm:"not null;column:class_id" json:"class_id"`
+	Linkfile  string `gorm:"column:link_file" json:"link_file"`
+	Amount    int64  `gorm:"column:amount" json:"amount"`
+	Status    string `gorm:"column:status" json:"status"`
+	CreatedAt string `gorm:"size:1000;null;column:created_at" json:"created_at"`
+	UpdatedAt string `gorm:"size:1000;null;column:updated_at" json:"updated_at"`
+	DeletedAt string `gorm:"size:1000;null;column:deleted_at" json:"deleted_at"`
+}
+
+func (pembayaran *Pembayaran) Save() (*Pembayaran, error) {
+	err := database.Database.Create(&pembayaran).Error
+	if err != nil {
+		return &Pembayaran{}, err
+	}
+	return pembayaran, nil
+}
